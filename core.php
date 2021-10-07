76d99f444bc66acee0f3ec87aa01877edf73697c
Galeno de Melo <galenodemelo@gmail.com>
2021-10-07 13:34:50 -0300

    Melhorias

--- core.php
+++ core.php
@@ -1,5 +1,5 @@
 <?php
-define('ZIP_PATH', __DIR__ . "/zip");
+define('ZIP_PATH', __DIR__ );
 
 $danfeCodes = explode(",", filter_input(INPUT_POST, "danfe-code"));
 $customerNames = explode(",", filter_input(INPUT_POST, "customer-name"));
@@ -10,7 +10,7 @@ $countFiles = count($files["name"]);
 $zipName = "NFE_Renomeadas_(" . date("d-m-Y H-i-s") . ").zip";
 $zipFile = ZIP_PATH . "/" . $zipName;
 $zip = new ZipArchive();
-$zip->open($zipFile, ZIPARCHIVE::CREATE);
+if (!$zip->open($zipFile, ZIPARCHIVE::CREATE)) exit("Não foi possível criar o arquivo ZIP");
 
 // Rename all the files
 for ($i = 0; $i < $countFiles; $i++) {
@@ -26,4 +26,5 @@ header("Content-type: application/zip");
 header("Content-disposition: filename={$zipName}");
 header("Content-length: {$fileLength}");
 readfile($zipFile);
+unlink($zipFile);
 exit;
